7c9117ce8e2c4ea141e99942bea0900bb97b6f45
Chongster <ftchong235@yahoo.com>
2020-02-03 15:20:10 +0800

    Updating the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
   };
 
   // constructor() {
@@ -11,7 +11,7 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
